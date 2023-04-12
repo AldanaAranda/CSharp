c55969a01c7604da4e270eb58aed3e30e598250f
AldanaAranda <aldanaaranda07@gmail.com>
2023-04-12 00:10:16 -0300

    subiendo ejercicio 1

---
+++ TerrenoRectangular/TerrenoRectangular/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace TerrenoRectangular
+{
+    partial class fTerreno
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bCalcular = new System.Windows.Forms.Button();
+            this.bCerrar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tDireccion = new System.Windows.Forms.TextBox();
+            this.tAncho = new System.Windows.Forms.TextBox();
+            this.tLargo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // bCalcular
+            // 
+            this.bCalcular.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.bCalcular.FlatAppearance.BorderSize = 0;
+            this.bCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bCalcular.Location = new System.Drawing.Point(157, 217);
+            this.bCalcular.Name = "bCalcular";
+            this.bCalcular.Size = new System.Drawing.Size(114, 29);
+            this.bCalcular.TabIndex = 0;
+            this.bCalcular.Text = "CALCULAR";
+            this.bCalcular.UseVisualStyleBackColor = false;
+            this.bCalcular.Click += new System.EventHandler(this.bCalcular_Click);
+            // 
+            // bCerrar
+            // 
+            this.bCerrar.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.bCerrar.FlatAppearance.BorderColor = System.Drawing.Color.CornflowerBlue;
+            this.bCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bCerrar.Location = new System.Drawing.Point(309, 217);
+            this.bCerrar.Name = "bCerrar";
+            this.bCerrar.Size = new System.Drawing.Size(114, 29);
+            this.bCerrar.TabIndex = 1;
+            this.bCerrar.Text = "CERRAR";
+            this.bCerrar.UseVisualStyleBackColor = false;
+            this.bCerrar.Click += new System.EventHandler(this.bCerrar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Direccion:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(72, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ancho:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(78, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Largo:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(286, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "metro/os";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(286, 136);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 25);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "metro/os";
+            // 
+            // tDireccion
+            // 
+            this.tDireccion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tDireccion.Location = new System.Drawing.Point(157, 32);
+            this.tDireccion.Multiline = true;
+            this.tDireccion.Name = "tDireccion";
+            this.tDireccion.Size = new System.Drawing.Size(266, 30);
+            this.tDireccion.TabIndex = 7;
+            // 
+            // tAncho
+            // 
+            this.tAncho.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tAncho.Location = new System.Drawing.Point(157, 85);
+            this.tAncho.Multiline = true;
+            this.tAncho.Name = "tAncho";
+            this.tAncho.Size = new System.Drawing.Size(123, 30);
+            this.tAncho.TabIndex = 8;
+            // 
+            // tLargo
+            // 
+            this.tLargo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tLargo.Location = new System.Drawing.Point(157, 136);
+            this.tLargo.Multiline = true;
+            this.tLargo.Name = "tLargo";
+            this.tLargo.Size = new System.Drawing.Size(123, 30);
+            this.tLargo.TabIndex = 9;
+            // 
+            // fTerreno
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(525, 342);
+            this.Controls.Add(this.tLargo);
+            this.Controls.Add(this.tAncho);
+            this.Controls.Add(this.tDireccion);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bCerrar);
+            this.Controls.Add(this.bCalcular);
+            this.Name = "fTerreno";
+            this.Text = "Terreno";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bCalcular;
+        private System.Windows.Forms.Button bCerrar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tDireccion;
+        private System.Windows.Forms.TextBox tAncho;
+        private System.Windows.Forms.TextBox tLargo;
+    }
+}
+
---
+++ TerrenoRectangular/TerrenoRectangular/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TerrenoRectangular
+{
+    public partial class fTerreno : Form
+    {
+        public fTerreno()
+        {
+            InitializeComponent();
+        }
+
+        private void bCerrar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void bCalcular_Click(object sender, EventArgs e)
+        {
+            string direccion = tDireccion.Text;
+            double ancho = Convert.ToDouble(tAncho.Text);
+            double largo = Convert.ToDouble(tLargo.Text);
+
+            double area = ancho * largo;
+
+            MessageBox.Show($"Terreno ubicado en: {direccion}\n" +
+                            $"Area: {area} metros cubicos");
+        }
+    }
+}
