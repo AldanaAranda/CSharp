5b70a6ea402e66217c5a481fd7e5faca947262df
AldanaAranda <aldanaaranda07@gmail.com>
2023-04-13 13:09:25 -0300

    actualizando

---
+++ ConversionGB/ConversionGB/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace ConversionGB
+{
+    partial class fConversion
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tArchivo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tDimension = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.bCalcular = new System.Windows.Forms.Button();
+            this.bCerrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre del archivo:";
+            // 
+            // tArchivo
+            // 
+            this.tArchivo.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tArchivo.Location = new System.Drawing.Point(231, 91);
+            this.tArchivo.Name = "tArchivo";
+            this.tArchivo.Size = new System.Drawing.Size(180, 23);
+            this.tArchivo.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(137, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tamaño:";
+            // 
+            // tDimension
+            // 
+            this.tDimension.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tDimension.Location = new System.Drawing.Point(231, 145);
+            this.tDimension.Name = "tDimension";
+            this.tDimension.Size = new System.Drawing.Size(100, 23);
+            this.tDimension.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(346, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "MB";
+            // 
+            // bCalcular
+            // 
+            this.bCalcular.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(241)))), ((int)(((byte)(74)))));
+            this.bCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bCalcular.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bCalcular.Location = new System.Drawing.Point(206, 211);
+            this.bCalcular.Name = "bCalcular";
+            this.bCalcular.Size = new System.Drawing.Size(94, 27);
+            this.bCalcular.TabIndex = 5;
+            this.bCalcular.Text = "CALCULAR";
+            this.bCalcular.UseVisualStyleBackColor = false;
+            this.bCalcular.Click += new System.EventHandler(this.bCalcular_Click);
+            // 
+            // bCerrar
+            // 
+            this.bCerrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(241)))), ((int)(((byte)(74)))));
+            this.bCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bCerrar.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bCerrar.Location = new System.Drawing.Point(393, 311);
+            this.bCerrar.Name = "bCerrar";
+            this.bCerrar.Size = new System.Drawing.Size(81, 27);
+            this.bCerrar.TabIndex = 6;
+            this.bCerrar.Text = "CERRAR";
+            this.bCerrar.UseVisualStyleBackColor = false;
+            this.bCerrar.Click += new System.EventHandler(this.bCerrar_Click);
+            // 
+            // fConversion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(195)))), ((int)(((byte)(211)))), ((int)(((byte)(80)))));
+            this.ClientSize = new System.Drawing.Size(498, 350);
+            this.Controls.Add(this.bCerrar);
+            this.Controls.Add(this.bCalcular);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tDimension);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tArchivo);
+            this.Controls.Add(this.label1);
+            this.Name = "fConversion";
+            this.Text = "Conversion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tArchivo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tDimension;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button bCalcular;
+        private System.Windows.Forms.Button bCerrar;
+    }
+}
+
---
+++ ConversionGB/ConversionGB/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConversionGB
+{
+    public partial class fConversion : Form
+    {
+        public fConversion()
+        {
+            InitializeComponent();
+        }
+
+        private void bCalcular_Click(object sender, EventArgs e)
+        {
+            string nombre = tArchivo.Text;
+            int mb = Convert.ToInt32(tDimension.Text);
+
+            int gb = mb / 1024;
+
+            MessageBox.Show($"El archivo {nombre} tiene {mb} MB \n" +
+                $"El tamaño del mismo en GB es: {gb}");
+        }
+
+        private void bCerrar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
