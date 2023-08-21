78b3fe215bf6e78771e02e0765477227797a496e
AldanaAranda <aldanaaranda07@gmail.com>
2023-08-21 13:11:34 -0300

    Subiendo otro ejercicio

---
+++ SumaTiempo/SumaTiempo/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace SumaTiempo
+{
+    partial class FTiempo
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tHora1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tMin1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tSeg1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tHora2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tMin2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tSeg2 = new System.Windows.Forms.TextBox();
+            this.bSumar = new System.Windows.Forms.Button();
+            this.bCerrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese la hora:";
+            // 
+            // tHora1
+            // 
+            this.tHora1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tHora1.Location = new System.Drawing.Point(200, 72);
+            this.tHora1.Name = "tHora1";
+            this.tHora1.Size = new System.Drawing.Size(92, 24);
+            this.tHora1.TabIndex = 1;
+            this.tHora1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(327, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ingrese los minutos:";
+            // 
+            // tMin1
+            // 
+            this.tMin1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tMin1.Location = new System.Drawing.Point(533, 72);
+            this.tMin1.Name = "tMin1";
+            this.tMin1.Size = new System.Drawing.Size(92, 24);
+            this.tMin1.TabIndex = 3;
+            this.tMin1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(661, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(213, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ingrese los segundos:";
+            // 
+            // tSeg1
+            // 
+            this.tSeg1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tSeg1.Location = new System.Drawing.Point(880, 72);
+            this.tSeg1.Name = "tSeg1";
+            this.tSeg1.Size = new System.Drawing.Size(92, 24);
+            this.tSeg1.TabIndex = 5;
+            this.tSeg1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(37, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(157, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Ingrese la hora:";
+            // 
+            // tHora2
+            // 
+            this.tHora2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tHora2.Location = new System.Drawing.Point(200, 174);
+            this.tHora2.Name = "tHora2";
+            this.tHora2.Size = new System.Drawing.Size(92, 24);
+            this.tHora2.TabIndex = 7;
+            this.tHora2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(327, 176);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(200, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Ingrese los minutos:";
+            // 
+            // tMin2
+            // 
+            this.tMin2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tMin2.Location = new System.Drawing.Point(533, 174);
+            this.tMin2.Name = "tMin2";
+            this.tMin2.Size = new System.Drawing.Size(92, 24);
+            this.tMin2.TabIndex = 9;
+            this.tMin2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(661, 176);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(213, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Ingrese los segundos:";
+            // 
+            // tSeg2
+            // 
+            this.tSeg2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tSeg2.Location = new System.Drawing.Point(880, 174);
+            this.tSeg2.Name = "tSeg2";
+            this.tSeg2.Size = new System.Drawing.Size(92, 24);
+            this.tSeg2.TabIndex = 11;
+            this.tSeg2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // bSumar
+            // 
+            this.bSumar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(198)))), ((int)(((byte)(234)))));
+            this.bSumar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bSumar.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bSumar.Location = new System.Drawing.Point(449, 279);
+            this.bSumar.Name = "bSumar";
+            this.bSumar.Size = new System.Drawing.Size(119, 32);
+            this.bSumar.TabIndex = 12;
+            this.bSumar.Text = "SUMAR";
+            this.bSumar.UseVisualStyleBackColor = false;
+            this.bSumar.Click += new System.EventHandler(this.bSumar_Click);
+            // 
+            // bCerrar
+            // 
+            this.bCerrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(198)))), ((int)(((byte)(234)))));
+            this.bCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bCerrar.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bCerrar.Location = new System.Drawing.Point(867, 345);
+            this.bCerrar.Name = "bCerrar";
+            this.bCerrar.Size = new System.Drawing.Size(92, 31);
+            this.bCerrar.TabIndex = 13;
+            this.bCerrar.Text = "CERRAR";
+            this.bCerrar.UseVisualStyleBackColor = false;
+            this.bCerrar.Click += new System.EventHandler(this.bCerrar_Click);
+            // 
+            // FTiempo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(137)))), ((int)(((byte)(176)))), ((int)(((byte)(184)))));
+            this.ClientSize = new System.Drawing.Size(1002, 401);
+            this.Controls.Add(this.bCerrar);
+            this.Controls.Add(this.bSumar);
+            this.Controls.Add(this.tSeg2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tMin2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tHora2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tSeg1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tMin1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tHora1);
+            this.Controls.Add(this.label1);
+            this.Name = "FTiempo";
+            this.Text = "Tiempo";
+            this.Load += new System.EventHandler(this.FTiempo_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tHora1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tMin1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tSeg1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tHora2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tMin2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tSeg2;
+        private System.Windows.Forms.Button bSumar;
+        private System.Windows.Forms.Button bCerrar;
+    }
+}
+
---
+++ SumaTiempo/SumaTiempo/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SumaTiempo
+{
+    public partial class FTiempo : Form
+    {
+        public FTiempo()
+        {
+            InitializeComponent();
+        }
+
+        private void bSumar_Click(object sender, EventArgs e)
+        {
+            int seg1 = Convert.ToInt16(tSeg1.Text);
+            int seg2 = Convert.ToInt16(tSeg2.Text);
+            int min1 = Convert.ToInt16(tMin1.Text);
+            int min2 = Convert.ToInt16(tMin2.Text);
+            int hora1 = Convert.ToInt16(tHora1.Text);
+            int hora2 = Convert.ToInt16(tHora2.Text);
+
+            int segundos = seg1 + seg2;
+            int pasoMin = segundos / 60;
+            int segTotal = segundos % 60;
+            int minutos = min1 + min2 + pasoMin;
+            int pasoHora = minutos / 60;
+            int minTotal = minutos % 60;
+            int hora = hora1 + hora2 + pasoHora;
+
+            MessageBox.Show($"La suma de los dos tiempos es: {hora}: {minTotal}: {segTotal}");
+        }
+
+        private void bCerrar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void FTiempo_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
