75e4dcf434c9823523376f53f2d665cb3d8e6921
AldanaAranda <aldanaaranda07@gmail.com>
2023-08-21 13:10:53 -0300

    Subiendo otro ejercicio

---
+++ SumaDigitos/SumaDigitos/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace SumaDigitos
+{
+    partial class FSuma
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tNumero = new System.Windows.Forms.TextBox();
+            this.bCalcular = new System.Windows.Forms.Button();
+            this.bCerrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(61, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(314, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese un número de dos cifras:";
+            // 
+            // tNumero
+            // 
+            this.tNumero.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tNumero.Location = new System.Drawing.Point(164, 113);
+            this.tNumero.Name = "tNumero";
+            this.tNumero.Size = new System.Drawing.Size(100, 26);
+            this.tNumero.TabIndex = 1;
+            // 
+            // bCalcular
+            // 
+            this.bCalcular.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(181)))), ((int)(((byte)(216)))), ((int)(((byte)(204)))));
+            this.bCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bCalcular.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bCalcular.Location = new System.Drawing.Point(164, 163);
+            this.bCalcular.Name = "bCalcular";
+            this.bCalcular.Size = new System.Drawing.Size(100, 29);
+            this.bCalcular.TabIndex = 2;
+            this.bCalcular.Text = "CALCULAR";
+            this.bCalcular.UseVisualStyleBackColor = false;
+            this.bCalcular.Click += new System.EventHandler(this.bCalcular_Click);
+            // 
+            // bCerrar
+            // 
+            this.bCerrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(181)))), ((int)(((byte)(216)))), ((int)(((byte)(204)))));
+            this.bCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bCerrar.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bCerrar.Location = new System.Drawing.Point(334, 251);
+            this.bCerrar.Name = "bCerrar";
+            this.bCerrar.Size = new System.Drawing.Size(86, 29);
+            this.bCerrar.TabIndex = 3;
+            this.bCerrar.Text = "CERRAR";
+            this.bCerrar.UseVisualStyleBackColor = false;
+            this.bCerrar.Click += new System.EventHandler(this.bCerrar_Click);
+            // 
+            // FSuma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(137)))), ((int)(((byte)(167)))), ((int)(((byte)(167)))));
+            this.ClientSize = new System.Drawing.Size(450, 303);
+            this.Controls.Add(this.bCerrar);
+            this.Controls.Add(this.bCalcular);
+            this.Controls.Add(this.tNumero);
+            this.Controls.Add(this.label1);
+            this.Name = "FSuma";
+            this.Text = "Suma";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tNumero;
+        private System.Windows.Forms.Button bCalcular;
+        private System.Windows.Forms.Button bCerrar;
+    }
+}
+
---
+++ SumaDigitos/SumaDigitos/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SumaDigitos
+{
+    public partial class FSuma : Form
+    {
+        public FSuma()
+        {
+            InitializeComponent();
+        }
+
+        private void bCalcular_Click(object sender, EventArgs e)
+        {
+            int num = Convert.ToInt16(tNumero.Text);
+
+            int cifra1 = num / 10;
+            int cifra2 = num % 10;
+
+            MessageBox.Show($"La suma de las cifras del numero es: {cifra1+cifra2}");
+        }
+
+        private void bCerrar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
